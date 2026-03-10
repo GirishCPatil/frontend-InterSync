ef094c8db528d62cf51a1e56dcc4bc064a6446e0
GirishCPatil <girishpatil621@gmail.com>
2026-03-10 10:52:22 +0530

    done

--- script/dashboard.js
+++ script/dashboard.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL = 'https://intersync.onrender.com';
 const token = localStorage.getItem('token');
 const user = JSON.parse(localStorage.getItem('user') || '{}');
 
